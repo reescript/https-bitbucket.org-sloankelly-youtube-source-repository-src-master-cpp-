a891e5b5cb78f68890c02ec2c91a8b04b6123377
Sloan Kelly <sloankelly@gmail.com>
2017-02-25 13:36:25 -0500

    Adding the knight sprite for testing player movement / speed.

---
+++ unity/Atic Atac Remake/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float x;
+    float y;
+    
+    [Tooltip("Speed in pixels / second")]
+    public float speed = 104;
+    
+    void Update()
+    {
+        x = x + Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        y = y + Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.position = new Vector3((int)x, (int)y, 0);
+    }
+}
