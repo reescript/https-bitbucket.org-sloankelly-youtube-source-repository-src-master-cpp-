5a11e96a0c87055cf59818714aabc0a7d97baa15
Sloan Kelly <sloankelly@gmail.com>
2017-04-16 14:33:21 -0400

    Custom Quality / Resolution Settings Screen

---
+++ unity/Custom Player Settings/Assets/Script/ButtonToNextScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonToNextScene : MonoBehaviour
+{
+    public string sceneName;
+
+    void Awake()
+    {
+        GetComponent<Button>().onClick.AddListener(new UnityAction(LoadScene));
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
