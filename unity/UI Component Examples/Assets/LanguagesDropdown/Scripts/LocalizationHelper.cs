5e3c34fedba6b7caf3c52d32a200a4bfd8f77b7c
Sloan Kelly <sloankelly@gmail.com>
2017-06-02 22:34:36 -0400

    Drop down list with images

---
+++ unity/UI Component Examples/Assets/LanguagesDropdown/Scripts/LocalizationHelper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Localization drop down example.
+/// </summary>
+public class LocalizationHelper : MonoBehaviour
+{
+    [Tooltip("The drop down box to populate with flags")]
+    public Dropdown dropDown;
+
+    [Tooltip("The flags of the languages that our game supports")]
+    public Sprite[] flags;
+
+    /// <summary>
+    /// Start this instance!
+    /// </summary>
+    void Start()
+    {
+        // Clear any existing options, just in case
+        dropDown.ClearOptions();
+
+        // Create the option list
+        List<Dropdown.OptionData> flagItems = new List<Dropdown.OptionData>();
+        
+        // Loop through each sprite
+        foreach (var flag in flags)
+        {
+            // Try and find the '.' in the sprite's name. This is used as a delimiter
+            // between the country code and the name of the language
+            string flagName = flag.name;
+            int dot = flag.name.IndexOf('.');
+            if (dot >= 0)
+            {
+                // Found? Then set the flag name to the characters to the right of the dot
+                flagName = flagName.Substring(dot + 1);
+            }
+            
+            // Add the option to the list
+            var flagOption = new Dropdown.OptionData(flagName, flag);
+            flagItems.Add(flagOption);
+        }
+
+        // Add the options to the drop down box
+        dropDown.AddOptions(flagItems);
+    }
+}
