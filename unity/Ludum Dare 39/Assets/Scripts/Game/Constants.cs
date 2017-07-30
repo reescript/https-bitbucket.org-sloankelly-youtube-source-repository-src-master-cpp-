a6c3a6475b3bc4664a0c358bda90a51998173be3
Sloan Kelly <sloankelly@gmail.com>
2017-07-29 21:09:20 -0400

    LDJam#39 Battery and radio power. Got in no oxygen death

--- unity/Ludum Dare 39/Assets/Scripts/Game/Constants.cs
+++ unity/Ludum Dare 39/Assets/Scripts/Game/Constants.cs
@@ -2,14 +2,20 @@
 {
     public static class Objects
     {
-        public static readonly int Floor = 0;
-        public static readonly int WayBelow = 1;
-        public static readonly int Tree = 2;
+        public const int Floor = 0;
+        public const int WayBelow = 1;
+        public const int Tree = 2;
+        public const int Battery = 3;
+        public const int Radio = 4;
     }
 
     public static class Energy
     {
         public static readonly int MovementCost = 5;
-        public static readonly float OxygenLoss = 1.5f;
+        public static readonly int MaxCarryBattery = 3;
+        public static readonly int Volts = 33;
+        public static readonly int VoltageLoss = 3;
+        public static readonly float OxygenLossDelay = 2f;
+        public static readonly float BatteryLossDelay = 3f;
     }
 }
--- unity/Ludum Dare 39/Assets/Scripts/Game/PlayerStatistics.cs
+++ unity/Ludum Dare 39/Assets/Scripts/Game/PlayerStatistics.cs
@@ -2,8 +2,33 @@
 {
     private int steps = 0;
     private int oxygen = 100;
+    private int batteries = 0;
+    private int radio = 0;
+    private bool isDead = false;
+
+    public bool IsDead { get { return isDead; } set { isDead = value; OnPropertyChanged("IsDead"); } }
+
+    public int Batteries { get { return batteries; } set { batteries = value; OnPropertyChanged("Batteries"); } }
 
     public int Steps { get { return steps; } set { steps = value; OnPropertyChanged("Steps"); } }
+
+    public int Radio
+    {
+        get { return radio; }
+        set
+        {
+            radio = value;
+            if (radio > 100)
+            {
+                radio = 100;
+            }
+            else if (radio < 0)
+            {
+                radio = 0;
+            }
+            OnPropertyChanged("Radio");
+        }
+    }
 
     public int Oxygen
     {
