b5f5d4b071f0ed308580a4f2806bb9016d687279
Sloan Kelly <sloankelly@gmail.com>
2017-07-29 21:51:08 -0400

    Main menu for LDJam#39

---
+++ unity/Ludum Dare 39/Assets/Scripts/MainMenu/FadeUpController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FadeUpController : MonoBehaviour
+{
+    public CanvasGroup canvas;
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        float alpha = 0f;
+        canvas.alpha = alpha;
+
+        while (alpha < 1f)
+        {
+            canvas.alpha = alpha;
+            alpha += Time.deltaTime;
+            yield return null;
+        }
+
+        canvas.alpha = 1f;
+    }
+}
---
+++ unity/Ludum Dare 39/Assets/Scripts/MainMenu/SceneLoadController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadController : MonoBehaviour
+{
+    public CanvasGroup mainMenu;
+    
+    internal void ShowInstructions()
+    {
+        StartCoroutine(LoadScene("Instructions"));
+    }
+
+    internal void StartGame()
+    {
+        StartCoroutine(LoadScene("Game"));
+    }
+
+    IEnumerator LoadScene(string sceneName)
+    {
+        float alpha = mainMenu.alpha;
+        while (alpha > 0)
+        {
+            mainMenu.alpha = alpha;
+            alpha -= Time.deltaTime;
+
+            yield return null;
+        }
+
+        mainMenu.alpha = 0f;
+
+        if (string.IsNullOrEmpty(sceneName))
+        {
+            Application.Quit();
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+    internal void Quit()
+    {
+        StartCoroutine(LoadScene(""));
+    }
+}
