17f1aa6aaf3b01e1950bf43d50fcb8666449800c
Sloan Kelly <sloankelly@gmail.com>
2017-11-08 21:12:52 -0500

    Build 1.0.4 (Alpha) - More cosmetic changes. Re-did buttons for main menu and added an "info" page.

---
+++ unity/Space Rescue/Space Rescue/Assets/Scripts/MainMenu/ClickToScene.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ClickToScene : MonoBehaviour
+{
+    public string nextScene;
+
+    void Awake()
+    {
+        var button = GetComponent<Button>();
+
+        Action launchGame = () =>
+        {
+            SceneManager.LoadScene(nextScene);
+        };
+
+        UnityAction actionWrapper = new UnityAction(launchGame);
+        button.onClick.AddListener(actionWrapper);
+    }
+}
