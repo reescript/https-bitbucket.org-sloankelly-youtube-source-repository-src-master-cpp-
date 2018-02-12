56a0631f3b48dcc19a35bc829131b6d3594d33be
Sloan Kelly <sloankelly@gmail.com>
2018-02-11 19:48:46 -0500

    Konami Code

---
+++ unity/Konami Code/Konami Code/Assets/Scripts/KonamiCode.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class KonamiCode : MonoBehaviour
+{
+    private const float WaitTime = 0.5f;
+
+    private KeyCode[] keys = new KeyCode[]
+    {
+        KeyCode.UpArrow,
+        KeyCode.UpArrow,
+        KeyCode.DownArrow,
+        KeyCode.DownArrow,
+        KeyCode.LeftArrow,
+        KeyCode.RightArrow,
+        KeyCode.LeftArrow,
+        KeyCode.RightArrow,
+        KeyCode.B,
+        KeyCode.A
+    };
+
+    public bool success;
+
+    IEnumerator Start ()
+    {
+        float timer = 0f;
+        int index = 0;
+
+        while (true)
+        {
+            if (Input.GetKeyDown(keys[index]))
+            {
+                index++;
+
+                if (index == keys.Length)
+                {
+                    success = true;
+                    timer = 0f;
+                }
+                else
+                {
+                    timer = WaitTime;
+                }
+            }
+
+            timer -= Time.deltaTime;
+            if (timer < 0)
+            {
+                timer = 0;
+                index = 0;
+            }
+
+            yield return null;
+        }
+	}
+}
