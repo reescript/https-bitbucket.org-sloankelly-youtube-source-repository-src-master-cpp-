a4bd4051fb247a7f15b6c32b6f620ee4d6f90bb2
Sloan Kelly <sloankelly@gmail.com>
2018-02-15 12:20:44 -0500

    Collision detection  - When the player's tick (enters) / leaves / contained Speed up the rotation of the tick Don't use rotation for the yellow ball  - Place at point  - Scale and change opacity   - Animation curves

--- unity/Lock Poppers/Lock Poppers/Assets/Scripts/RotateBall.cs
+++ unity/Lock Poppers/Lock Poppers/Assets/Scripts/RotateBall.cs
@@ -6,30 +6,50 @@ public class RotateBall : MonoBehaviour
 {
     float duration = 1f;
 
-    public void StartRotation(Action finishedRotation, float direction = 1)
+    public AnimationCurve scaleIn;
+
+    public AnimationCurve scaleOut;
+
+    public AnimationCurve opacityIn;
+
+    public AnimationCurve opacityOut;
+
+    public void StartFadeIn(Action finishedFade, float duration, float direction)
     {
-        StartCoroutine(RotateMe(finishedRotation, direction));
+        StartCoroutine(DoFade(finishedFade, scaleIn, opacityIn, duration, direction, true));
     }
 
-    IEnumerator RotateMe(Action finishedRotation, float direction)
+    public void StartFadeOut(Action finishedFade, float duration, float direction)
     {
-        Quaternion current = transform.rotation;
+        StartCoroutine(DoFade(finishedFade, scaleOut, opacityOut, duration, direction));
+    }
 
-        Quaternion target = Quaternion.Euler(0, 0, 90 + UnityEngine.Random.Range(0, 180) * direction);
+    IEnumerator DoFade(Action finishedFade, AnimationCurve scaleCurve, AnimationCurve opacityCurve, float duration, float direction, bool moveBall = false)
+    {
+        if (moveBall)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 90 + UnityEngine.Random.Range(0, 180) * direction);
+        }
+
+        var spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        var ballTransform = spriteRenderer.transform;
 
         float time = 0f;
-
         while (time < 1f)
         {
-            transform.rotation = Quaternion.Lerp(current, target, time);
-            time += Time.deltaTime / duration;
+            float currentScale = scaleCurve.Evaluate(time);
+            float currentOpacity = opacityCurve.Evaluate(time);
 
+            ballTransform.localScale = new Vector3(currentScale, currentScale, currentScale);
+            spriteRenderer.color = new Color(1, 1, 1, currentOpacity);
+
+            time += Time.deltaTime / duration;
             yield return null;
         }
 
-        if (finishedRotation != null)
+        if (finishedFade != null)
         {
-            finishedRotation();
+            finishedFade();
         }
     }
 }
--- unity/Lock Poppers/Lock Poppers/Assets/Scripts/RotateTick.cs
+++ unity/Lock Poppers/Lock Poppers/Assets/Scripts/RotateTick.cs
@@ -1,16 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class RotateTick : MonoBehaviour
 {
+    public float angleSpeed = -45f;
 
-    float angleSpeed = -45f;
-    
-    void Update()
+    private TickTrigger tickTrigger;
+
+    public Action missedTheBall;
+
+    public float direction = 1f;
+
+    public bool InsideBall { get { return tickTrigger == null ? false : tickTrigger.InsideBall; } }
+
+    public void Reset()
     {
-        transform.Rotate(Vector3.forward, angleSpeed * Time.deltaTime);
+        tickTrigger.Reset();
     }
 
+    void Awake()
+    {
+        tickTrigger = GetComponentInChildren<TickTrigger>();
+        tickTrigger.tickExit = MissedTheBall;
+    }
 
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, angleSpeed * Time.deltaTime * direction);
+    }
+
+    void MissedTheBall()
+    {
+        if (missedTheBall != null)
+        {
+            missedTheBall();
+        }
+    }
 }
