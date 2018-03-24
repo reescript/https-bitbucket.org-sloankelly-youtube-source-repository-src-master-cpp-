e1a26e01a4c475fd30668d1b2928a311b8caddac
Sloan Kelly <sloankelly@gmail.com>
2018-03-24 12:49:54 -0400

    Subtitles (setup)

---
+++ unity/Subtitles/Subtitles/Assets/Scripts/PlayVoiceOver.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayVoiceOver : MonoBehaviour
+{
+    private AudioSource audioSource;
+    
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            audioSource.Play();
+        }
+    }
+}
